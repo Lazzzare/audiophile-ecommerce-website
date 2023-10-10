81c88908e2bf55e211e58c483cdf1bfb937e149a
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-10 18:12:23 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,33 @@ import Cart from "../images/shared/icon-cart.svg";
 import Hamburger from "../images/shared/tablet/icon-hamburger.svg";
 // import Close from "../images/shared/tablet/icon-close-menu.svg";
 
+const menuItemsArray = ["Home", "HEADPHONES", "SPEAKERS", "EARPHONES"];
+
 const Navbar = () => {
   return (
-    <div className="relative flex items-center justify-between px-6 py-9 bg-darkBlack">
-      <img src={Hamburger} alt="Hamburger" className="cursor-pointer" />
-      <img src={Logo} alt="Logo" className="cursor-pointer" />
-      <img src={Cart} alt="Cart" className="cursor-pointer" />
-    </div>
+    <>
+      <div className="flex items-center  justify-between px-6 py-9 bg-black max-w-[1110px] mx-auto">
+        <div className="flex items-center md:gap-10 gap-0 w-full lg:w-fit">
+          <img
+            src={Hamburger}
+            alt="Hamburger"
+            className="cursor-pointer lg:hidden"
+          />
+          <img
+            src={Logo}
+            alt="Logo"
+            className="cursor-pointer items-center mx-auto md:mx-0"
+          />
+        </div>
+        <ul className="hidden lg:flex justify-center mx-auto gap-8 items-center text-white">
+          {menuItemsArray.map((item, index) => {
+            return <li key={index}>={item}</li>;
+          })}
+        </ul>
+        <img src={Cart} alt="Cart" className="cursor-pointer" />
+      </div>
+      <hr className="w-full h-[1px] text-white bg-white md:w-full mx-auto lg:w-[1110px]" />
+    </>
   );
 };
 
