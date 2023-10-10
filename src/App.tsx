b4684ebd61a21e3221a94a8b9c1abe610991fe20
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-10 14:15:56 +0400

    initial commit (react, typescript, tailwindcss)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      orange: "#D87D4A",
+      darkBlack: "#101010",
+      silver: "#F1F1F1",
+      whiteSmoke: "#FAFAFA",
+      lightOrange: "#FBAF85",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+  },
+  plugins: [],
+};
