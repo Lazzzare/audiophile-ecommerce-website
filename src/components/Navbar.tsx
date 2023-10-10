5678f472ff054276ea90a9af759e956ae3ae6c52
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-10 14:27:57 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,16 @@
+import Logo from "../../images/shared/audiophile-logo.svg";
+import Cart from "../../images/shared/icon-cart.svg";
+import Hamburger from "../../images/shared/tablet/icon-hamburger.svg";
+// import Close from "../../images/shared/tablet/icon-close-menu.svg";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="px-6 py-9 bg-darkBlack">
+      <img src={Hamburger} alt="Hamburger" />
+      <img src={Logo} alt="Logo" />
+      <img src={Cart} alt="Cart" />
+    </div>
+  );
 };
 
 export default Navbar;
