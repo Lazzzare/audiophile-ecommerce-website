1d5fc423863c2bbd968036902790ceb94417c117
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-10 15:11:19 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
-import Logo from "../../images/shared/audiophile-logo.svg";
-import Cart from "../../images/shared/icon-cart.svg";
-import Hamburger from "../../images/shared/tablet/icon-hamburger.svg";
-// import Close from "../../images/shared/tablet/icon-close-menu.svg";
+import Logo from "../images/shared/audiophile-logo.svg";
+import Cart from "../images/shared/icon-cart.svg";
+import Hamburger from "../images/shared/tablet/icon-hamburger.svg";
+// import Close from "../images/shared/tablet/icon-close-menu.svg";
 
 const Navbar = () => {
   return (
-    <div className="px-6 py-9 bg-darkBlack">
-      <img src={Hamburger} alt="Hamburger" />
-      <img src={Logo} alt="Logo" />
-      <img src={Cart} alt="Cart" />
+    <div className="relative flex items-center justify-between px-6 py-9 bg-darkBlack">
+      <img src={Hamburger} alt="Hamburger" className="cursor-pointer" />
+      <img src={Logo} alt="Logo" className="cursor-pointer" />
+      <img src={Cart} alt="Cart" className="cursor-pointer" />
     </div>
   );
 };
