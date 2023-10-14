a50e34b2d848ee9ade568c5cdcbd0aabb704f5d5
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-14 18:30:33 +0400

    Create Products.tsx

---
+++ src/components/Products.tsx
@@ -0,0 +1,20 @@
+import Headphones from "../images/shared/headphones.png";
+import Speakers from "../images/shared/speakers.png";
+import Earphones from "../images/shared/earphones.png";
+
+const Products = [
+  {
+    title: "Headphones",
+    image: Headphones,
+  },
+  {
+    title: "Speakers",
+    image: Speakers,
+  },
+  {
+    title: "Earphones",
+    image: Earphones,
+  },
+];
+
+export default Products;
