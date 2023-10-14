a4cc242d2fc889a0c642d786eea5342146e04842
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-14 15:37:06 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import Home from "./pages/Home";
 import Headphones from "./pages/Headphones";
 import Speakers from "./pages/Speakers";
 import Earphones from "./pages/Earphones";
+import { useState } from "react";
 
 const App = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
   return (
-    <div className="w-full h-screen overflow-x-hidden">
-      <Navbar />
+    <div
+      className={`w-full h-screen overflow-x-hidden ${
+        mobileMenu ? "overflow-hidden" : null
+      }`}
+    >
+      <Navbar mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/headphones" element={<Headphones />}></Route>
