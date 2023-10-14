23f3860c9aed1b8bfe55b76d262613a6a82cf1b6
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-14 18:30:31 +0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import Hamburger from "../images/shared/tablet/icon-hamburger.svg";
 import Logo from "../images/shared/audiophile-logo.svg";
 import Cart from "../images/shared/icon-cart.svg";
 import Close from "../images/shared/tablet/icon-close-menu.svg";
+import Arrow from "../images/shared/arrow.svg";
+import Shadow from "../images/shared/shadow.png";
 import { Link } from "react-router-dom";
 import { menuListArray } from "../components/menuListArray";
-// import { useState } from "react";
-// import HomeShop from "./HomeShop";
+import Products from "./Products";
 
 interface Props {
   mobileMenu: boolean;
@@ -14,7 +15,7 @@ interface Props {
 
 const Navbar = ({ mobileMenu, setMobileMenu }: Props) => {
   return (
-    <div className={`bg-black`}>
+    <div className="bg-black">
       <div className="lg:max-w-[1440px] flex lg:mx-auto justify-between items-center bg-black px-6 md:px-[39px] lg:px-[165px] py-8 lg:py-9">
         {mobileMenu ? (
           <img
@@ -33,8 +34,41 @@ const Navbar = ({ mobileMenu, setMobileMenu }: Props) => {
         )}
         {/* MobileMenu */}
         {mobileMenu ? (
-          <div className="absolute opacity-40  top-0 left-0 right-0 bg-black w-full h-screen">
-            <div className="w-full h-[730px] "></div>
+          <div className="absolute z-20 top-0 left-0 right-0 bg-black w-full h-screen lg:hidden">
+            <div className="w-full h-[730px] bg-white mt-[90px] pt-[80px] md:pt-[200px] md:gap-20 md:px-5 rounded-b-2xl flex flex-col md:flex-row">
+              {Products.map((product, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="w-[327px] h-[165px] mb-[68px] rounded-lg bg-silver mx-auto flex flex-col items-center justify-center"
+                  >
+                    <div className="flex-col mt-[-4rem]">
+                      <img
+                        src={product.image}
+                        alt={product.title}
+                        className="w-20 lg:w-[120px]"
+                      />
+                      <img
+                        src={Shadow}
+                        alt="Shadow"
+                        className="w-20 h-[14px]"
+                      />
+                    </div>
+                    <div className="mt-2 lg:mt-8 space-y-4">
+                      <h1 className="text-black text-[15px]  font-bold tracking-[1px] uppercase">
+                        {product.title}
+                      </h1>
+                      <div className="flex gap-3 items-center mx-auto justify-center cursor-pointer">
+                        <h2 className="text-black text-[13px] font-bold tracking-[1px] uppercase opacity-50 z-10">
+                          SHOP
+                        </h2>
+                        <img src={Arrow} alt="Arrow" />
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         ) : null}
         <img src={Logo} alt="Logo" className="cursor-pointer z-30" />
