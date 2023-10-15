597285742bd4676e9e139dbc2db14e7c415b9421
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-15 15:25:07 +0400

    Update Headphones.tsx

--- src/pages/Headphones.tsx
+++ src/pages/Headphones.tsx
@@ -2,9 +2,12 @@
 // import FooterHero from "../components/FooterHero";
 // import HomeShop from "../components/HomeShop";
 
+import HeadphonesTitle from "../components/HeadphonesTitle";
+
 const Headphones = () => {
   return (
     <div className="">
+      <HeadphonesTitle />
       {/* <HomeShop />
       <FooterHero />
       <Footer /> */}
