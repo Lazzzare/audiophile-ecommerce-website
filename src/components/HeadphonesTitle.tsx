97b6ab4de380cd30e3b975b69499fbdcfb95a5e7
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-15 15:24:57 +0400

    Create HeadphonesTitle.tsx

---
+++ src/components/HeadphonesTitle.tsx
@@ -0,0 +1,11 @@
+const HeadphonesTitle = () => {
+  return (
+    <div className="py-[90px] w-full bg-black mx-auto flex justify-center items-center">
+      <h1 className="text-white text-[40px] font-bold uppercase tracking-[1.5px]">
+        HEADPHONES
+      </h1>
+    </div>
+  );
+};
+
+export default HeadphonesTitle;
