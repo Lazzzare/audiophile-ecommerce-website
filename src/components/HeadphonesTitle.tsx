ec8bf376d136e004db015994519d511ad85c5767
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-16 21:13:39 +0400

    Update HeadphonesTitle.tsx

--- src/components/HeadphonesTitle.tsx
+++ src/components/HeadphonesTitle.tsx
@@ -1,7 +1,7 @@
 const HeadphonesTitle = () => {
   return (
-    <div className="py-[90px] w-full bg-black mx-auto flex justify-center items-center">
-      <h1 className="text-white text-[40px] font-bold uppercase tracking-[1.5px]">
+    <div className="py-8 md:py-[105px] lg:py-[90px] w-full bg-black mx-auto flex justify-center items-center">
+      <h1 className="text-white text-[28px] md:text-[40px] font-bold uppercase tracking-[2px] lg:tracking-[1.5px]">
         HEADPHONES
       </h1>
     </div>
