f8f7086372de83c31679509489cdd40cc7f72737
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-17 11:56:45 +0400

    Create SpeakersTitle.tsx

---
+++ src/components/SpeakersTitle.tsx
@@ -0,0 +1,11 @@
+const SpeakersTitle = () => {
+  return (
+    <div className="py-8 md:py-[105px] lg:py-[90px] w-full bg-black mx-auto flex justify-center items-center">
+      <h1 className="text-white text-[28px] md:text-[40px] font-bold uppercase tracking-[2px] lg:tracking-[1.5px]">
+        SPEAKERS
+      </h1>
+    </div>
+  );
+};
+
+export default SpeakersTitle;
