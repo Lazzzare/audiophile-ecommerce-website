dab7a8dbbfe1021cdc46227694b5f052a50a464b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-17 11:56:50 +0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,13 @@
-import Footer from "../components/Footer";
-import FooterHero from "../components/FooterHero";
 import HomeHero from "../components/HomeHero";
 import HomeProducts from "../components/HomeProducts";
 import HomeShop from "../components/HomeShop";
 
-interface Props {
-  activeMenuRoute: number | null;
-  setActiveMenuRoute: (e: number) => void;
-}
-
-const Home = ({ activeMenuRoute, setActiveMenuRoute }: Props) => {
+const Home = () => {
   return (
     <div>
       <HomeHero />
       <HomeShop />
       <HomeProducts />
-      <FooterHero />
-      <Footer
-        activeMenuRoute={activeMenuRoute}
-        setActiveMenuRoute={setActiveMenuRoute}
-      />
     </div>
   );
 };
