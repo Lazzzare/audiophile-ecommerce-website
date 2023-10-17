62e331238e1fc87526fdd9352d11e46b68caf3ea
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-17 11:56:47 +0400

    Update Headphones.tsx

--- src/pages/Headphones.tsx
+++ src/pages/Headphones.tsx
@@ -1,26 +1,13 @@
-import Footer from "../components/Footer";
-import FooterHero from "../components/FooterHero";
 import HomeShop from "../components/HomeShop";
-
 import HeadphonesProduct from "../components/HeadphonesProduct";
 import HeadphonesTitle from "../components/HeadphonesTitle";
 
-interface Props {
-  activeMenuRoute: number | null;
-  setActiveMenuRoute: (e: number) => void;
-}
-
-const Headphones = ({ activeMenuRoute, setActiveMenuRoute }: Props) => {
+const Headphones = () => {
   return (
     <div className="">
       <HeadphonesTitle />
       <HeadphonesProduct />
       <HomeShop />
-      <FooterHero />
-      <Footer
-        activeMenuRoute={activeMenuRoute}
-        setActiveMenuRoute={setActiveMenuRoute}
-      />
     </div>
   );
 };
