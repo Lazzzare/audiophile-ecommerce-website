84c98d3c9dcd5ac7e138df792eddd4a75b9afc8d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-17 12:06:21 +0400

    Update Earphones.tsx

--- src/pages/Earphones.tsx
+++ src/pages/Earphones.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import EarphoneTitle from "../components/EarphoneTitle";
+import EarphoneProducts from "../components/EarphoneProducts";
+import HomeShop from "../components/HomeShop";
 
 const Earphones = () => {
-  return <div>Earphones</div>;
+  return (
+    <div>
+      <EarphoneTitle />
+      <EarphoneProducts />
+      <HomeShop />
+    </div>
+  );
 };
 
 export default Earphones;
