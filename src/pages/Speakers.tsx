3cd2a27d19826ae72216ba0e7e892f403e55eff2
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-18 11:32:15 +0400

    Update Speakers.tsx

--- src/pages/Speakers.tsx
+++ src/pages/Speakers.tsx
@@ -1,4 +1,4 @@
-import HomeShop from "../components/HomeShop";
+// import HomeShop from "../components/HomeShop";
 import SpeakersProduct from "../components/SpeakersProduct";
 import SpeakersTitle from "../components/SpeakersTitle";
 
@@ -7,7 +7,7 @@ const Speakers = () => {
     <div>
       <SpeakersTitle />
       <SpeakersProduct />
-      <HomeShop />
+      {/* <HomeShop /> */}
     </div>
   );
 };
