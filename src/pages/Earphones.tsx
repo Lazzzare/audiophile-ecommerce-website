5d89adb551074a10f7f3879135cabb828122f6db
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-18 11:32:11 +0400

    Update Earphones.tsx

--- src/pages/Earphones.tsx
+++ src/pages/Earphones.tsx
@@ -1,13 +1,13 @@
 import EarphoneTitle from "../components/Earphones/EarphoneTitle";
 import EarphoneProducts from "../components/Earphones/EarphoneProducts";
-import HomeShop from "../components/HomeShop";
+// import HomeShop from "../components/HomeShop";
 
 const Earphones = () => {
   return (
     <div>
       <EarphoneTitle />
       <EarphoneProducts />
-      <HomeShop />
+      {/* <HomeShop /> */}
     </div>
   );
 };
