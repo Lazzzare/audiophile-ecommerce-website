43df676cd7edb8d45ddacd69b811e4d75c302db2
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-24 19:30:52 +0400

    Update HeadphoneFirstProduct.tsx

--- src/components/Headphones/HeadphoneFirstProduct.tsx
+++ src/components/Headphones/HeadphoneFirstProduct.tsx
@@ -1,7 +1,136 @@
+import { Link } from "react-router-dom";
+import ProductMobile from "../../images/product-xx99-mark-two-headphones/mobile/image-product.jpg";
+import ProductTablet from "../../images/product-xx99-mark-two-headphones/tablet/image-product.jpg";
+import ProductDesktop from "../../images/product-xx99-mark-two-headphones/desktop/image-product.jpg";
+
 const HeadphoneFirstProduct = () => {
   return (
-    <div>
-      <h2>HelloProduct 1</h2>
+    <div className="px-6 md:px-10 lg:px-0 pt-4 lg:pt-[80px] md:pt-10 lg:max-w-[1110px] lg:mx-auto">
+      <Link to={"/headphones"}>
+        <h5 className="text-black text-[15px] font-medium leading-[25px] opacity-50 cursor-pointer">
+          Go Back
+        </h5>
+      </Link>
+      <div className="flex flex-col mt-6 md:flex-row w-full md:gap-[70px] lg:gap-[125px] md:justify-center md:items-center">
+        <div className="mb-8">
+          <img
+            src={ProductMobile}
+            alt="ProductImage"
+            className="flex md:hidden lg:hidden"
+          />
+          <img
+            src={ProductTablet}
+            alt="ProductImage"
+            className="hidden md:flex lg:hidden"
+          />
+          <img
+            src={ProductDesktop}
+            alt="ProductImage"
+            className="hidden md:hidden lg:flex"
+          />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col gap-6">
+            <h2 className="text-orange text-sm lg:text-[14px] tracking-[10px]">
+              NEW PRODUCT
+            </h2>
+            <h1 className="text-black text-[28px] font-bold tracking-[1px] uppercase md:leading-8">
+              XX99 Mark II Headphones
+            </h1>
+            <p className="text-black text-[15px] md:w-[339px] lg:w-[445px] font-medium leading-[25px] opacity-50">
+              The new XX99 Mark II headphones is the pinnacle of pristine audio.
+              It redefines your premium headphone experience by reproducing the
+              balanced depth and precision of studio-quality sound.
+            </p>
+          </div>
+          {/* Price and add to cart */}
+          <div className="flex flex-col gap-8 mt-6">
+            <h4 className="text-lg font-bold tracking-[1.3px] text-black">
+              $ 2,999
+            </h4>
+            <div className="flex flex-row gap-4">
+              <div className="flex flex-row justify-between p-[15px] w-[120px] bg-silver text-black text-[13px] font-bold tracking-[1px]">
+                <span className="cursor-pointer opacity-50">-</span>
+                <h6>1</h6>
+                <span className="cursor-pointer opacity-50">+</span>
+              </div>
+              <button className="text-white text-[13px] font-bold tracking-[1px] uppercase bg-orange w-[160px] py-[15px] hover:bg-lightOrange duration-500">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Features and in the box */}
+      <div className="flex flex-col gap-y-[88px] lg:flex-row lg:w-full mt-[88px] lg:gap-[125px]">
+        {/* Features */}
+        <div className="">
+          <h1 className="text-black text-2xl md:text-[32px] font-bold leading-9 tracking-[0.8px] md:tracking-[1.1px] mb-6">
+            FEATURES
+          </h1>
+          <p className="text-black text-[15px] lg:w-[635px] font-medium leading-[25px] opacity-50">
+            Connect via Bluetooth or nearly any wired source. This speaker
+            features optical, digital coaxial, USB Type-B, stereo RCA, and
+            stereo XLR inputs, allowing you to have up to five wired source
+            devices connected for easy switching. Improved bluetooth technology
+            offers near lossless audio quality at up to 328ft (100m). Discover
+            clear, more natural sounding highs than the competition with ZX9’s
+            signature planar diaphragm tweeter. Equally important is its
+            powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass
+            unit. You’ll be able to enjoy equal sound quality whether in a large
+            room or small den. Furthermore, you will experience new sensations
+            from old songs since it can respond to even the subtle waveforms.
+          </p>
+        </div>
+        {/* In the box */}
+        <div className="flex flex-col md:flex-row lg:flex-col lg:justify-normal md:justify-between">
+          <h1 className="text-black text-2xl md:text-[36px] font-bold leading-9 tracking-[0.8px] md:tracking-[1.1px] mb-6 uppercase">
+            in the box
+          </h1>
+          <div>
+            <div className="flex flex-row gap-5">
+              <span className="text-orange text-[15px] font-bold leading-[25px]">
+                2x
+              </span>
+              <p className="text-black text-[15px] font-medium leading-[25px] opacity-50">
+                Speaker Unit
+              </p>
+            </div>
+            <div className="flex flex-row gap-5">
+              <span className="text-orange text-[15px] font-bold leading-[25px]">
+                2x
+              </span>
+              <p className="text-black text-[15px] font-medium leading-[25px] opacity-50">
+                Speaker Cloth Panel
+              </p>
+            </div>
+            <div className="flex flex-row gap-5">
+              <span className="text-orange text-[15px] font-bold leading-[25px]">
+                1x
+              </span>
+              <p className="text-black text-[15px] font-medium leading-[25px] opacity-50">
+                User Manual
+              </p>
+            </div>
+            <div className="flex flex-row gap-5">
+              <span className="text-orange text-[15px] font-bold leading-[25px]">
+                1x
+              </span>
+              <p className="text-black text-[15px] font-medium leading-[25px] opacity-50">
+                3.5mm 10m Audio Cable
+              </p>
+            </div>
+            <div className="flex flex-row gap-5">
+              <span className="text-orange text-[15px] font-bold leading-[25px]">
+                1x
+              </span>
+              <p className="text-black text-[15px] font-medium leading-[25px] opacity-50">
+                10m Optical Cable
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
