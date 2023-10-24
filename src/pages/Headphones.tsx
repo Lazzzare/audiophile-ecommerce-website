7b50a5e9f9535fbcbae2eb1e9b83adce795278ec
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-24 18:17:54 +0400

    Update Headphones.tsx

--- src/pages/Headphones.tsx
+++ src/pages/Headphones.tsx
@@ -1,9 +1,5 @@
-import { Route, Routes } from "react-router-dom";
 import HeadphonesTitle from "../components/Headphones/HeadphonesTitle";
 import HeadphonesProduct from "../components/Headphones/HeadphonesProduct";
-import HeadphoneFirstProduct from "../components/Headphones/HeadphoneFirstProduct";
-import HeadphonesSecondProduct from "../components/Headphones/HeadphonesSecondProduct";
-import HeadphonesLastProduct from "../components/Headphones/HeadphonesLastProduct";
 
 const Headphones = () => {
   return (
@@ -11,11 +7,6 @@ const Headphones = () => {
       <HeadphonesTitle />
       <HeadphonesProduct />
       {/* <HomeShop /> */}
-      <Routes>
-        <Route path="product1" element={<HeadphoneFirstProduct />} />
-        <Route path="product2" element={<HeadphonesSecondProduct />} />
-        <Route path="product3" element={<HeadphonesLastProduct />} />
-      </Routes>
     </>
   );
 };
