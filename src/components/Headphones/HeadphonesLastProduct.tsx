a519e4336b4aa24ae62c5030782cdf261471941f
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-25 15:44:46 +0400

    Update HeadphonesLastProduct.tsx

--- src/components/Headphones/HeadphonesLastProduct.tsx
+++ src/components/Headphones/HeadphonesLastProduct.tsx
@@ -20,9 +20,13 @@ import ZX9SpeakerDesktop from "../../images/shared/desktop/image-zx9-speaker.jpg
 
 interface Props {
   setActiveMenuRoute: (e: number) => void;
+  productAmount: number;
 }
 
-const HeadphonesLastProduct = ({ setActiveMenuRoute }: Props) => {
+const HeadphonesLastProduct = ({
+  setActiveMenuRoute,
+  productAmount,
+}: Props) => {
   return (
     <div className="px-6 md:px-10 lg:px-0 pt-4 lg:pt-[80px] md:pt-10 lg:max-w-[1110px] lg:mx-auto">
       <Link to={"/headphones"}>
@@ -70,7 +74,7 @@ const HeadphonesLastProduct = ({ setActiveMenuRoute }: Props) => {
             <div className="flex flex-row gap-4">
               <div className="flex flex-row justify-between p-[15px] w-[120px] bg-silver text-black text-[13px] font-bold tracking-[1px]">
                 <span className="cursor-pointer opacity-50">-</span>
-                <h6>1</h6>
+                <h6>{productAmount}</h6>
                 <span className="cursor-pointer opacity-50">+</span>
               </div>
               <button className="text-white text-[13px] font-bold tracking-[1px] uppercase bg-orange w-[160px] py-[15px] hover:bg-lightOrange duration-500">
