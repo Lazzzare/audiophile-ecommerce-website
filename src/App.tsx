405f241bf9f529a7913e520d58bf1df3b98cae01
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-31 16:52:15 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ const App = () => {
 
   return (
     <div
-      className={`relative w-full h-screen bg-[#FAFAFA]  ${
+      className={`relative w-full h-screen bg-[#FAFAFA] overflow-x-hidden  ${
         mobileMenu ? "" : ""
       }`}
     >
